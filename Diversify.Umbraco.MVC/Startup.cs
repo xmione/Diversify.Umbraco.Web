bc649af582a61a96923ec5a6a8cb29714d1bd1cb
Solomio S. Sisante <solomiosisante@hotmail.com>
2018-08-29 10:45:19 +0800

    Grid Content is now displaying.

---
+++ Diversify.Umbraco.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Diversify.Umbraco.MVC.Startup))]
+namespace Diversify.Umbraco.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
