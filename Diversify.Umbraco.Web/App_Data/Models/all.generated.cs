8df608683e01d8e9890b7cc6f870c9ebbd46546c
Solomio S. Sisante <solomiosisante@hotmail.com>
2018-08-29 12:57:55 +0800

    With Bread crumbs using Top Navigation

--- Diversify.Umbraco.Web/App_Data/Models/all.generated.cs
+++ Diversify.Umbraco.Web/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "84cddfbbc5c2f880")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.6")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
---
+++ Diversify.Umbraco.Web/Model/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Diversify.Umbraco.Web.Model
+{
+    public static class HtmlExtensions
+    {
+
+        public static IHtmlString Breadcrumb(this HtmlHelper helper, IPublishedContent thisPage)
+        {
+            StringBuilder breadcrumb = new StringBuilder();
+
+            IEnumerable<IPublishedContent> ancestors = thisPage.Ancestors().Reverse();
+
+            foreach (IPublishedContent page in ancestors)
+            {
+                breadcrumb.Append($"<a href=\"{page.Url}\">{page.Name}</a>");
+            }
+
+            breadcrumb.Append($"<span>{thisPage.Name}</span>");
+
+            return MvcHtmlString.Create(breadcrumb.ToString());
+        }
+     
+
+    }
+}
