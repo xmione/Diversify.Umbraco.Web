f0e33ed00831aab5fd1eaa48bfc9ff80f4ab1dde
Solomio S. Sisante <solomiosisante@hotmail.com>
2018-08-31 17:11:15 +0800

    Fixed Deployment to IIS. Removed log4net errors. Included necessary files and folders.

---
+++ Diversify.Umbraco.Web/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Diversify.Umbraco.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(object));
+            log.Fatal("Unhandled Exception", Server.GetLastError());
+
+            Server.ClearError();
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Diversify.Umbraco.Web/obj/Debug/Package/PackageTmp/App_Plugins/SolGridEditor/SolGridEditor.controller.js
@@ -0,0 +1,3 @@
+﻿angular.module("umbraco").controller("SolGridEditorController", function ($scope) {
+    $scope.control.value = "my new value";
+});
