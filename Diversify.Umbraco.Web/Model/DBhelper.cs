50309215427b8655ad0b85c49233a284ba3086e6
Solomio S. Sisante <solomiosisante@hotmail.com>
2018-08-31 19:03:08 +0800

    Added Entity Framework
    Added Entity Framework

---
+++ Diversify.Umbraco.Web/Model/DBhelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Diversify.Umbraco.Web.Model
+{
+    public class DBHelper
+    {
+        private string _connectionString;
+        public DBHelper()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DiversifyDB"].ConnectionString;
+        }
+
+
+    }
+}
---
+++ Diversify.Umbraco.Web/dbo.Images.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Images]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FileName] NTEXT NULL, 
+    [Content] NTEXT NULL
+)
